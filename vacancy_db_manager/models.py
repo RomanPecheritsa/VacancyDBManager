0b0afbf58308607a22103cee55b9f59a90ab6a31
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-06 11:44:10 +0300

    add docs model Employer

--- vacancy_db_manager/models.py
+++ vacancy_db_manager/models.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel
 
 
 class Employer(BaseModel):
+    """
+    Represents an employer with basic details such as ID, name, and the number of open vacancies.
+    Attributes:
+        id (str): The unique identifier for the employer.
+        name (str): The name of the employer.
+        open_vacancies (int): The number of open vacancies the employer currently has.
+    """
     id: str
     name: str
     open_vacancies: int
