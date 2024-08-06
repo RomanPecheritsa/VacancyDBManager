99ec8ab94105094ffd4ac35f5c4768b976b52d9e
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-06 12:15:45 +0300

    add func get_employers_ids

--- vacancy_db_manager/utils.py
+++ vacancy_db_manager/utils.py
@@ -3,7 +3,7 @@ from typing import List
 from vacancy_db_manager.models import Employer
 
 
-def json_to_data_employers(employers_data) -> List[Employer]:
+def json_to_data_employers(employers_data: List[dict]) -> List[Employer]:
     """
     Converts a list of dictionaries with employer data into a list of objects of the Employer model.
 
@@ -16,3 +16,19 @@ def json_to_data_employers(employers_data) -> List[Employer]:
         List[Employer]: A list of objects of the Employer model created based on the input data.
     """
     return [Employer(**item) for item in employers_data]
+
+
+def get_employers_ids(data_employers: List[Employer]) -> List[str]:
+    """
+    Extracts the IDs of employers from a list of Employer objects.
+    This function takes a list of Employer instances and returns a list containing
+    the IDs of these employers. It is useful for retrieving the unique identifiers
+    of employers for further processing or querying.
+    Args:
+        data_employers (List[Employer]): A list of Employer objects from which the IDs
+                                         are to be extracted.
+    Returns:
+        List[str]: A list of strings, each representing an employer ID. If the input
+                   list is empty, an empty list is returned.
+    """
+    return [item.id for item in data_employers]
