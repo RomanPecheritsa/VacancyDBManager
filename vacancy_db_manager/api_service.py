a4dd7fab01b815d107b65acc9032162e592776c6
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-06 11:40:04 +0300

    add docs, typing

--- vacancy_db_manager/api_service.py
+++ vacancy_db_manager/api_service.py
@@ -5,7 +5,19 @@ from typing import List
 BASE_URL = "https://api.hh.ru/employers"
 
 
-def get_employers() -> List:
+def get_employers() -> List[dict]:
+    """
+    Fetch a list of IT employers with open vacancies from the HeadHunter API.
+
+    This function sends a GET request to the HeadHunter API to retrieve a list of employers
+    who have open vacancies in the IT sector. The function sorts the results by the number
+    of open vacancies and limits the number of results to 10. The locale for the search is set to "RU".
+
+    Returns:
+        List[dict]: A list of dictionaries where each dictionary represents an employer
+                    with details such as id, name, alternate_url, and open_vacancies.
+                    Returns an empty list if the request fails.
+    """
     params = {
         "text": 'IT',
         "only_with_vacancies": True,
