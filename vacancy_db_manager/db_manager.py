6d3d161264a5eb0206f5db5b5c2760fe75f05d48
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-07 16:11:44 +0300

    changed comments

---
+++ vacancy_db_manager/db_manager.py
@@ -0,0 +1,48 @@
+import psycopg2
+from psycopg2 import sql
+from prettytable import PrettyTable
+from config import logger
+
+
+class DBManager:
+    def __init__(self, config):
+        self.config = config
+
+    def __enter__(self):
+        self.conn = psycopg2.connect(**self.config)
+        self.cursor = self.conn.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            logger.error(f'Произошла ошибка {exc_type} {exc_val}')
+        self.cursor.close()
+        self.conn.close()
+
+    def get_companies_and_vacancies_count(self):
+        """
+        Gets a list of all companies and the number of vacancies each company has.
+        Returns:
+            PrettyTable: A formatted table containing the company name and the count of vacancies.
+        """
+        query = """
+        SELECT e.employer_name, COUNT(v.vacancy_id) as vacancies_count
+        FROM employers e
+        LEFT JOIN vacancies v ON e.employer_id = v.employer_id
+        GROUP BY e.employer_name
+        ORDER BY vacancies_count DESC;
+        """
+        try:
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+
+            table = PrettyTable()
+            table.field_names = ["company", "vacancies count"]
+
+            for row in result:
+                table.add_row(row)
+
+            return table
+        except Exception as e:
+            logger.error(f"Ошибка при поиске компаний и подсчете вакансий: {e}")
+            raise
